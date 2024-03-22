d17a0a65aac699809506fabad54e6137c8bc4c72
LEE JIEUN <jieun3595@soongsil.ac.kr>
2024-03-22 19:52:52 +0900

    2024.03.22 - 스프링입문 section 2

--- src/main/java/hello/HelloSpring/controller/HelloController.java
+++ src/main/java/hello/HelloSpring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.HelloSpring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,5 +13,39 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String hellomvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // 그냥 body에 대입 태그들 없음
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    // json으로 반환 -> key-value 방식
+    // 객체가 오면 default json 방식, http 응답에 반환
+    public Hello helloapi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
