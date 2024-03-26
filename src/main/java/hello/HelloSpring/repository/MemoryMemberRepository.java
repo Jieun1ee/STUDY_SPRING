6798b812e5a9c6b874f522cf7198fdd8f38f5eeb
LEE JIEUN <jieun3595@soongsil.ac.kr>
2024-03-26 23:38:27 +0900

    2024.03.26 - 스프링입문 section 4

--- src/main/java/hello/HelloSpring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/HelloSpring/repository/MemoryMemberRepository.java
@@ -6,7 +6,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 
     // 동시성 문제 고려되지 않음. 단순하게 구현함
